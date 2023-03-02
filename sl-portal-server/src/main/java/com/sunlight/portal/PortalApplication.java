a182607a0b4e911ff07a0813bc2c4317aaae7e6e
gaoguang <gaoguang0605@163.com>
2023-03-02 18:50:40 +0800

    nacos register successfully...

--- sl-portal-server/src/main/java/com/sunlight/portal/PortalApplication.java
+++ sl-portal-server/src/main/java/com/sunlight/portal/PortalApplication.java
@@ -1,11 +1,19 @@
 package com.sunlight.portal;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@MapperScan("com.sunlight.portal.accounts.dao")
+@ComponentScan(value="com.sunlight.portal.*")
 public class PortalApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PortalApplication.class, args);
+        try {
+            SpringApplication.run(PortalApplication.class, args);
+        } catch (Throwable ex) {
+            ex.printStackTrace();
+        }
     }
 }
